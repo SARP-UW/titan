8e4b6b90c63a229fb7027f9cf140694bb08d8ff6
Aaron425 <amcbri@uw.edu>
2025-02-07 17:04:00 -0800

    build system work

--- template.h
+++ template.h
@@ -1,5 +1,5 @@
 /**
- * This file is part of the Titan HAL
+ * This file is part of the titan library.
  * Copyright (c) 2025 UW SARP
  * 
  * This program is free software: you can redistribute it and/or modify
@@ -15,45 +15,22 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * 
  * @internal
- * @file src/STM32H745_CM7/dma.h
- * @authors Aaron McBride
- * @brief Internal DMA driver.
+ * @file TODO
+ * @authors TODO
+ * @brief TODO
  */
 
-// Invalidate D-Cache info: https://community.st.com/t5/stm32-mcus/dma-is-not-working-on-stm32h7-devices/ta-p/49498
-
-
 #pragma once
-#include <stdint.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include "src/STM32H745_CM7/mmio.h"
-#include "src/STM32H745_CM7/interrupt.h" 
+// includes here
 
 #ifdef __cplusplus
   extern "C" {
 #endif
 
   /************************************************************************************************
-   * Implementation Resources
+   * TODO
    ************************************************************************************************/
-
-  void _init_dma(void) {
-    
-  }
-
-  /************************************************************************************************
-   * Internal Functions
-   ************************************************************************************************/
-
-  /************************************************************************************************
-   * DMA Driver Functions
-   ************************************************************************************************/
-
-
 
 #ifdef __cplusplus
   } // extern "C"
 #endif
-
-/** @endinternal */
