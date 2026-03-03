dbbb3a305e4e4a9e948225466bf69546b3d7431c
Jude Merritt <jm@Judes-MacBook-Air-559.local>
2026-03-03 12:22:36 -0800

    mangetometer.h draft

--- src/devices/magnetometer.h
+++ src/devices/magnetometer.h
@@ -14,9 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * @file devices/imu.h
+ * @file devices/magnetometer.h
  * @authors Jude Merritt
- * @brief ICM-42688-P IMU driver implementation
+ * @brief IMMC5983MA magnetometer implementation
  */
 
 #include "internal/mmio.h"
@@ -25,36 +25,34 @@
 
 #pragma once
 
-// IMU device struct
-struct imu_spi_dev {
+// Magnetometer device struct
+struct magnetometer_spi_dev {
     uint8_t inst;   // SPI Instance
     uint8_t ss_pin; // Slave Select Pin
 };
 
 struct result { 
-    int16_t accel_x; 
-    int16_t accel_y;
-    int16_t accel_z;
-    int16_t gyro_x;
-    int16_t gyro_y;
-    int16_t gyro_z;
-    int16_t temp; // Internal temperature
+    int32_t mag_x; 
+    int32_t mag_y;
+    int32_t mag_z;
+    float temperature; // Internal temperature
+
 };
 
 /**
- * @brief Initializes the ICM-42688-P IMU. 
+ * @brief Initializes the IMMC5983MA Magnetometer. 
  * 
- * @param dev  pointer to the imu_spi_dev structure. 
+ * @param dev  pointer to the magnetometer_spi_dev structure. 
  */
-void imu_init(struct imu_spi_dev* dev);
+void magnetometer_init(struct magnetometer_spi_dev* dev);
 
 /**
  * @brief Performs a burst read of the sensor registers and returns them 
  *        by updating the result structure. Ensure that the desired SPI
  *        instance has been initialized before calling this function. 
  * 
- * @param dev  pointer to the imu_spi_dev structure. 
+ * @param dev  pointer to the magnetometer_spi_dev structure. 
  * @param result  pointer to the result structure. 
  * @return 1 if the parameters are valid and -1 if not. 
  */
-int imu_transfer(struct imu_spi_dev* dev, struct result* result);
+int magnetometer_read(struct magnetometer_spi_dev* dev, struct result* result);
