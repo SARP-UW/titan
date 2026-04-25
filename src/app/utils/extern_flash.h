18617e49165d3f9fc50432ea0af0386df5eee795
Jude Merritt <jm@Judes-MacBook-Air-1052.local>
2026-04-25 03:10:23 -0700

    extern flash program partially complete

--- src/app/utils/extern_flash.h
+++ src/app/utils/extern_flash.h
@@ -4,18 +4,29 @@
 
 #pragma once
 
-#define STATE_POOL_BASE_ADDR 0x00000000 // 496 writes of space at 256 bytes per write
-uint32_t curr_state_addr;
+#define STATE_POOL_BASE_ADDR 0x00000000 
+volatile uint32_t* const state_addr_ptr = (volatile uint32_t*)0x38800000; // Address of current state
 
-#define DATA_POOL_BASE_ADDR 0x1F0000000 // 30,750 writes of space at 256 bytes per write
-uint32_t curr_data_addr;
+#define DATA_POOL_BASE_ADDR 0x00000200
+volatile uint32_t* const data_addr_ptr = (volatile uint32_t*)0x38800004; // Address of current data log
+volatile uint32_t* const curr_page_space_ptr = (volatile uint32_t*)0x38800008; // Remaining space in current page
 
-enum data_type {
-    STATE, 
-    DATA
+enum states_t {
+    ARMED_STATE = 0x01,
+    FILL_STATE = 0x02,
+    FIRE_STATE = 0x03,
+    HOLD_STATE = 0x04,
+    INIT_STATE = 0x05,
+    SAFE_STATE = 0x06,
+    STANDBY_STATE = 0x07
 };
 
 void init_extern_flash();
 
+void log_state(enum states_t state, enum ti_errc_t* errc);
 
-void write_extern_flash(enum data_type type, uint8_t* data, uint8_t length, enum ti_errc_t *errc);
+enum states_t get_prev_state(enum ti_errc_t* errc);
+
+bool check_saved_state();
+
+void log_data(uint8_t* data, uint16_t length, enum ti_errc_t* errc);
