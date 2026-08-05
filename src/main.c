4ddf429c116a6cdb1cbd4a6b7cabb4b448eba159
Joshua Beard <matinzzz@icloud.com>
2026-08-04 18:17:50 -0700

    .

--- src/main.c
+++ src/main.c
@@ -8,15 +8,19 @@
 #include "peripheral/errc.h"
 #include "peripheral/gpio.h"
 #include "peripheral/qspi.h"
+#include "app/utils/pinout.h"
 #include "peripheral/systick.h"
 
 
 
 // NOLINTNEXTLINE(readability-identifier-naming)
-void _start() { // NOLINT(misc-use-internal-linkage)
-    uint32_t pin = 99;
+void _start() { // NOLINT(misc-use-internal-linkage)3
 
-    tal_set_mode(pin, 1);
-    tal_set_pin(pin, 1);
-    tal_pull_pin(pin, 1);
+    int a = 10;
+    tal_enable_clock(BATT_EN);
+    tal_set_mode(BATT_EN, 1);
+    asm("BKPT #0");
+    tal_set_pin(BATT_EN, 1);
+    tal_pull_pin(BATT_EN, 1);
+    
 }
